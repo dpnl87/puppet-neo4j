3a4d8191bcbf781084a70433bdcac3633e38d084
Daniel Paulus <d.paulus@gmail.com>
2014-05-22 14:11:11 +0200

    Provide a way to install neo4j with a custom file package

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,11 +8,15 @@
 #   Explanation of what this parameter affects and what it defaults to.
 #
 class neo4j (
+  $install_from_file = undef,
+  $install_from_repo = undef,
   $package_name = $neo4j::params::package_name,
   $service_name = $neo4j::params::service_name,
 ) inherits neo4j::params {
 
-  # validate parameters here
+  if !$install_from_repo and !$install_from_file {
+    fail('You need to specify a way for package installation.')
+  }
 
   class { 'neo4j::install': } ->
   class { 'neo4j::config': } ~>
--- manifests/install.pp
+++ manifests/install.pp
@@ -2,7 +2,28 @@
 #
 class neo4j::install {
 
-  package { $neo4j::package_name:
-    ensure => present,
+  # If 'install_from_file' set to true use rpm file to install
+  if $neo4j::install_from_file {
+
+    file { "/tmp/${neo4j::package_name}":
+      source => "puppet:///modules/neo4j/files/${neo4j::package_name}",
+    }
+
+    package { $neo4j::package_name:
+      ensure   => present,
+      provider => 'rpm',
+      source   => "/tmp/${neo4j::package_name}",
+      require  => File["/tmp/${neo4j::package_name}"],
+    }
+  }
+
+  # If 'install_from_repo' set to true use repository to install
+  if $neo4j::install_from_repo {
+
+    package { $neo4j::package_name:
+      ensure  => present,
+      require => Yumrepo[$neo4j::repo_name],
+    }
+
   }
 }
